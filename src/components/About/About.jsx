67fcf67881eff04de801b58746f2d118b53b98f7
Harshvardhan <harshingh28@gmail.com>
2025-08-14 18:32:39 +0530

    final

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -48,7 +48,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1o0Ezf3iRCaqOkiqh-2RJysY_3ug4NYG7/view?usp=sharing"
+            href="https://drive.google.com/file/d/1afmpyCAhRla71LmLYizYIGWn2IGpbu-0/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
